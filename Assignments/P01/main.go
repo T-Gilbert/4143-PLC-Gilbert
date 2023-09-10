b541e9db6e74756aa4d232da36106efe040baf99
Trevor Gilbert <122938102+T-Gilbert@users.noreply.github.com>
2023-09-10 14:22:56 -0500

    Update main.go

--- Assignments/P01/main.go
+++ Assignments/P01/main.go
@@ -1,1 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/T-Gilbert/4143-PLC-Gilbert/tree/main/Assignments/P01/mascot"
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(mascot.BestMascot())
+	fmt.Println(quote.Go())
+}
 
