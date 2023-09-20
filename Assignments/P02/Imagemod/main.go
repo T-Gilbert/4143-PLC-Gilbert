90b207006f864afd8ee304d962421c4ad36aa6c3
Trevor Gilbert <122938102+T-Gilbert@users.noreply.github.com>
2023-09-20 18:29:59 -0500

    Update main.go

--- Assignments/P02/Imagemod/main.go
+++ Assignments/P02/Imagemod/main.go
@@ -1,21 +1,20 @@
+
 package main
 
 import (
 	"fmt"
 	"myimageapp/imagemod"
+	"github.com/T-Gilbert/4143-PLC-Gilbert/tree/main/Assignments/P02/Imagemod"
 )
 
 func main() {
-	// Create an ImageManipulator instance with an existing image
-	im, err := imagemod.NewImageManipulatorWithImage("mustangs.jpg")
+	im, err := imagemod.NewImageManipulatorWithImage(LazyBear.jpg")
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
-	// Draw a rectangle
 	im.DrawRectangle(150, 50, 560, 411)
 
-	// Save the modified image to a file
-	im.SaveToFile("mustangs_edited.png")
+	im.SaveToFile("LazyBear.png")
 }
