f4abd2a98cd584335f10e100c0e96985e194942c
Trevor Gilbert <122938102+T-Gilbert@users.noreply.github.com>
2023-09-20 18:30:33 -0500

    Update randpackage.go

--- Assignments/P02/Imagemod/randpackage.go
+++ Assignments/P02/Imagemod/randpackage.go
@@ -1,11 +1,12 @@
+
 package imagemod
 
 import (
-	"github.com/fogleman/gg"
+	"github.com/T-Gilbert/4143-PLC-Gilbert/tree/main/Assignments/P02"
 )
 
 // ImageManipulator represents an image manipulation tool.
 type ImageManipulator struct {
-	Image     *gg.Context
+	Image     *P02.Context
 	ImagePath string // Add a field to store the image path
 }
